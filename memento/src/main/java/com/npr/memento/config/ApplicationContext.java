e4446d34e098fb455a4d36c293b85ebafea64e23
Ravindra Desai <ravindradesai042@gmail.com>
2018-12-24 17:55:23 +0530

    Memento Project Initial Checkin

---
+++ memento/src/main/java/com/npr/memento/config/ApplicationContext.java
@@ -0,0 +1,11 @@
+package com.npr.memento.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.npr.memento.*")
+public class ApplicationContext {
+
+	
+}
